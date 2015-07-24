2a55ca0f70e99886097b824dc4406afbae083ddc
Kristin Beltz <beltz08@me.com>
2015-07-24 14:06:54 -0500

    Add current_user sessions to users_helper.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  # sets the current user or redirects to the login screen
+  #
+  # returns the current user or redirects
+  def current_user
+    if session[:user_id]
+      @user = User.find(session[:user_id].to_i)
+    else
+      session.clear
+      redirect "/login"
+    end
+  end
 end
