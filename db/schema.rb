d2af9cfeefb2fb00f04a96f51be79e9e87a35e93
Kristin Beltz <beltz08@me.com>
2015-07-24 11:54:03 -0500

    DB merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150724155928) do
+ActiveRecord::Schema.define(version: 20150724164514) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,15 @@ ActiveRecord::Schema.define(version: 20150724155928) do
     t.string   "url"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "pedestrians", force: :cascade do |t|
+    t.integer  "female"
+    t.integer  "male"
+    t.integer  "disabled"
+    t.integer  "other"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
