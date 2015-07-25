d31115961289dfe7a167b559f59174b56552fcd4
Ciprianna <ciprianna17@gmail.com>
2015-07-25 11:18:04 -0500

    Finished instructions view text. Added route and controller   method.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -90,6 +90,9 @@ class UsersController < ApplicationController
     end
   end
 
+  def instructions
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
