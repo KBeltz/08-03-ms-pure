fcbfccae47fa7e535d0de4c3a03db223618c2ed2
Kristin Beltz <beltz08@me.com>
2015-07-27 13:05:20 -0500

    Remove new resource button from cyclists and pedestrians in active admin.

--- app/admin/cyclist.rb
+++ app/admin/cyclist.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Cyclist do
-  actions :all, except: [:create, :edit, :destroy]
+  actions :all, except: [:create, :edit, :destroy, :new]
   batch_action :destroy, false
 
   permit_params :female_with_helmet, :female_no_helmet, :female_sidewalk, :female_wrong_way, :male_with_helmet, :male_no_helmet, :male_sidewalk, :male_wrong_way
