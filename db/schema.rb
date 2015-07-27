27eb74ee3f424a2eff997cc042d72292df814f26
Ciprianna <ciprianna17@gmail.com>
2015-07-27 12:43:24 -0500

    DB migration after weather table created.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727163702) do
+ActiveRecord::Schema.define(version: 20150727174034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,6 @@ ActiveRecord::Schema.define(version: 20150727163702) do
     t.integer  "male_wrong_way"
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
-    t.integer  "location_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -75,9 +74,8 @@ ActiveRecord::Schema.define(version: 20150727163702) do
     t.integer  "male"
     t.integer  "disabled"
     t.integer  "other"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -87,6 +85,10 @@ ActiveRecord::Schema.define(version: 20150727163702) do
     t.integer  "location_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "weathers", force: :cascade do |t|
+    t.string "weather"
   end
 
 end
