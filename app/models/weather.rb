6c53bf5e5abc30524279078075cb354ba0ff98af
Kristin Beltz <beltz08@me.com>
2015-07-27 13:07:34 -0500

    Rake db:migrate

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,3 +1,11 @@
+# == Schema Information
+#
+# Table name: weathers
+#
+#  id      :integer          not null, primary key
+#  weather :string
+#
+
 class Weather < ActiveRecord::Base
   validates :weather, presence: true
 end
