8a4dddb65826d4bbd1f04a2c7df1ed8e1251b7bb
Ciprianna <ciprianna17@gmail.com>
2015-07-27 15:34:01 -0500

    Created db migration file to change date and time fields to start   time and end time fields that are type datetime.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150727181827) do
+ActiveRecord::Schema.define(version: 20150727202807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,10 +82,10 @@ ActiveRecord::Schema.define(version: 20150727181827) do
     t.integer  "user_id"
     t.integer  "location_id"
     t.integer  "weather_id"
-    t.time     "time"
-    t.date     "date"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "users", force: :cascade do |t|
