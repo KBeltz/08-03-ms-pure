53c9918fbb0146c0cec548408a16d83004586229
Ciprianna <ciprianna17@gmail.com>
2015-07-27 16:14:08 -0500

    Converted shifts_controller back to default and added new columns   as permitted. Changed form, index, and show files for the new   db structure.

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -1,74 +1,8 @@
-class ShiftsController < ApplicationController
-  before_action :set_shift, only: [:show, :edit, :update, :destroy]
-
-  # GET /shifts
-  # GET /shifts.json
-  def index
-    @shifts = Shift.all
-  end
-
-  # GET /shifts/1
-  # GET /shifts/1.json
-  def show
-  end
-
-  # GET /shifts/new
-  def new
-    @shift = Shift.new
-  end
-
-  # GET /shifts/1/edit
-  def edit
-  end
-
-  # POST /shifts
-  # POST /shifts.json
-  def create
-    @shift = Shift.new(shift_params)
-
-    respond_to do |format|
-      if @shift.save
-        format.html { redirect_to @shift, notice: 'Shift was successfully created.' }
-        format.json { render :show, status: :created, shift: @shift }
-      else
-        format.html { render :new }
-        format.json { render json: @shift.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /shifts/1
-  # PATCH/PUT /shifts/1.json
-  def update
-    respond_to do |format|
-      if @shift.update(location_params)
-        format.html { redirect_to @shift, notice: 'Shift was successfully updated.' }
-        format.json { render :show, status: :ok, shift: @shift }
-      else
-        format.html { render :edit }
-        format.json { render json: @shift.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /shifts/1
-  # DELETE /shifts/1.json
-  def destroy
-    @shift.destroy
-    respond_to do |format|
-      format.html { redirect_to shifts_url, notice: 'Shift was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+class ShiftsController < InheritedResources::Base
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_location
-      @shift = Shift.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def shift_params
-      params.require(:shift).permit(:user_id, :location_id, :weather_id, :time, :date)
+      params.require(:shift).permit(:user_id, :location_id, :weather_id, :start_time, :end_time)
     end
 end
