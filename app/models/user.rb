db2c135b3ab6169187236822fdb151517ab674e8
Kristin Beltz <beltz08@me.com>
2015-07-28 11:49:47 -0500

    Change validation in user model from username to first_name and last_name to reflect database column changes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,8 +19,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-         
-  validates :username, presence: true
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   validates :location_id, presence: true
