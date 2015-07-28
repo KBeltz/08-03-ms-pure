ca58a0896863d2289ca44efcdfa988eba9147669
Kristin Beltz <beltz08@me.com>
2015-07-28 12:05:13 -0500

    Attempt to add a before action to validate user with devise, but it isn't yet functional.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # before_action :check_login
+  # before_action :authenticate_user!
 
   # def check_login
   #   if session[:user_id] == nil
