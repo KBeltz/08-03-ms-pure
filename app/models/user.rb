618b4adbb27219ed8d895063513736851c25e378
Kristin Beltz <beltz08@me.com>
2015-07-28 12:16:44 -0500

    Generate devise for user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,13 +12,28 @@
 #  reset_password_sent_at :datetime
 #  first_name             :string
 #  last_name              :string
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # devise :database_authenticatable,
+  #        :recoverable, :rememberable, :trackable, :validatable
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,6 +12,17 @@
 #  reset_password_sent_at :datetime
 #  first_name             :string
 #  last_name              :string
+#  remember_created_at    :datetime
+#  sign_in_count          :integer          default(0), not null
+#  current_sign_in_at     :datetime
+#  last_sign_in_at        :datetime
+#  current_sign_in_ip     :inet
+#  last_sign_in_ip        :inet
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
 
 require 'test_helper'
