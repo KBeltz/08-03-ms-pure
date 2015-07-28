defd614bad6e0786685c865194c5a9cb0ad6f678
Ciprianna <ciprianna17@gmail.com>
2015-07-28 10:55:16 -0500

    Updated users table; removed "username", added "first_name" and   "last_name" columns.

---
+++ db/migrate/20150728155302_update_user_table.rb
@@ -0,0 +1,8 @@
+class UpdateUserTable < ActiveRecord::Migration
+  def change
+    remove_column :users, :username, :string
+
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+  end
+end
