5c0c491735419193360a6095f66848b55f1ada6e
Kristin Beltz <beltz08@me.com>
2015-07-29 15:16:42 -0500

    Remove location_id validation from user model. Remove location from user profile view.

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,7 +34,6 @@ class User < ActiveRecord::Base
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
-  validates :location_id, presence: true
 
   has_many :shifts
 end
