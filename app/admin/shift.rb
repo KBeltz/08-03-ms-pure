c4b1cf4641e2014870402d60fb02163cd5a90b46
Kristin Beltz <beltz08@me.com>
2015-07-30 09:04:39 -0500

    I think just whitespace removal.

--- app/admin/shift.rb
+++ app/admin/shift.rb
@@ -2,6 +2,17 @@ ActiveAdmin.register Shift do
   menu priority: 5
   permit_params :user_id, :location_id, :weather_id, :start_time, :end_time
 
+  form do |f|
+    f.inputs do
+      f.input :user, :member_label => :last_name
+      f.input :location, :member_label => :location_name
+      f.input :start_time
+      f.input :end_time
+    end
+    f.button :Submit
+  end
+
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
