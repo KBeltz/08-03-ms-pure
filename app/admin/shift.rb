3e53c4912c7a2a49163b5f8618c90977ed09e474
Kristin Beltz <beltz08@me.com>
2015-08-03 10:37:06 -0500

    Edit shift data input

--- app/admin/shift.rb
+++ app/admin/shift.rb
@@ -2,18 +2,21 @@ ActiveAdmin.register Shift do
   menu priority: 5
   permit_params :user_id, :location_id, :weather_id, :start_time, :end_time
 
-  filter :weather_id, :as => :select, :collection => Shift.all.collect {|o| [o.weather_id]}
-  filter :user_id, :as => :select, :collection => Shift.all.collect {|o| [o.user_id]}
-  filter :location_id, :as => :select, :collection => Shift.all.collect {|o| [o.location_id]}
+  filter :weather_id do
+    Weather.find(weather_id)
+
+  end
+  filter :user_id
+  filter :location_id
 
   form do |f|
     f.inputs do
       f.input :user
       f.input :location, :member_label => :location_name
       f.input :start_time, :as => :datepicker, label: "Shift Start Date"
-      f.input :start_time, :as => :time_picker
+      f.input :start_time, :as => :time_picker, :input_html => { :step => :hour }
       f.input :end_time, :as => :datepicker, label: "Shift End Date"
-      f.input :end_time, :as => :time_picker
+      f.input :end_time, :as => :time_picker, :input_html => { :step => :hour }
     end
     f.button :Submit
   end
