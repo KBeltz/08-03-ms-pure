12a0a8d67d9e18b70756e4a2d6251830e65773d5
Kristin Beltz <beltz08@me.com>
2015-08-03 14:05:45 -0500

    Edit application controller to address admin login issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || "/profile"
+    request.env['omniauth.origin'] || stored_location_for(resource) ||
+    if resource.is_a?(User)
+      "/profile"
+    else
+      super
+    end
   end
 
 end
